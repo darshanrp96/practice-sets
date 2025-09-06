29d6b28b469b8c38bd0b12b42a4583227bcc8b0b
darshan <darshanpardeshi8@gmail.co>
2025-09-06 22:46:41 +0530

    class added

---
+++ class.js
@@ -0,0 +1,20 @@
+console.log("class");
+class Practice{
+
+constructor(name,id){
+
+    this.name=name;
+    this.id=id;
+}
+}
+
+let name = new Practice("darshan",101);
+let name1 = new Practice("manali",102);
+
+console.log(name);
+console.log(name1);
+
+
+
+
+
