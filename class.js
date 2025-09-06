a0b8468d477e911877c0529f4aa59e254577f549
darshan <darshanpardeshi8@gmail.co>
2025-09-06 22:49:26 +0530

    class updated

--- class.js
+++ class.js
@@ -8,11 +8,17 @@ constructor(name,id){
 }
 }
 
-let name = new Practice("darshan",101);
-let name1 = new Practice("manali",102);
+let emp1 = new Practice("darshan",101);
+let emp2 = new Practice("manali",102);
 
-console.log(name);
-console.log(name1);
+console.log(emp1.name);
+console.log(emp1.id);
+
+console.log(emp2.name);
+console.log(emp2.id);
+
+
+
 
 
 
