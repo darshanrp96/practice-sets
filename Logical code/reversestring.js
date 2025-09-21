0e1f588932523db8019c19cfeee409e27aecd975
darshan <darshanpardeshi8@gmail.co>
2025-09-21 12:12:55 +0530

    reverse number added

--- Logical code/reversestring.js
+++ Logical code/reversestring.js
@@ -10,3 +10,14 @@ for (let i = str1.length -1 ; i>=0 ; i--){
 
 console.log(revstr);
 
+//---------------------------------
+console.log("reverse number");
+let revnum= 8485838107;
+let rev = "";
+let strnum = revnum.toString();
+
+for( let revnum= strnum.length-1; revnum>=0; revnum--){
+    rev = rev + strnum[revnum];
+}
+
+console.log(rev);
