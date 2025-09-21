dd01e20796f222432cb77f861990d62930298acb
darshan <darshanpardeshi8@gmail.co>
2025-09-21 12:06:34 +0530

    revserse string added

---
+++ Logical code/reversestring.js
@@ -0,0 +1,12 @@
+console.log("reverse strings");
+
+let str1 = "olleh";
+let revstr = "";
+
+for (let i = str1.length -1 ; i>=0 ; i--){
+    revstr = revstr + str1[i];
+
+}
+
+console.log(revstr);
+
